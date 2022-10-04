fb2feda48749c965f416080b65a5b6466c58b0a8
ANKIT GURWAN <109846346+AnkitGurwan@users.noreply.github.com>
2022-10-04 22:12:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+import React, {useState,useEffect} from 'react';
+import {toast} from "react-toastify";
+import {getNames} from "./api";
+import { EditOutlined, DeleteOutlined} from "@ant-design/icons";
+import {Link} from "react-router-dom";
+import FormElement from "./Form";
+import Loading from "./Loading";
+const Crud=()=>
+{
+    const [name,setName]=useState("");
+    const [Loading,setLoading]=useState(false);
+    const [names,setNames]=useState([]);
+
+    useEffect(()=>{
+        loadNames();
+    },[]);
+    const loadNames=()=>
+        getNames().then((name)=>setNames(name.data));
+    return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-8">
+                         {Loading ? (<Loading/>) :
+                         (
+                          <>  
+                         <h4 className="text-center">BYE</h4>
+                        <FormElement handleSubmit={handleSubmit}
+                        name={name}
+                        setName={setName}/>                                 
+                        {names && names.map((t)=>(
+                            <div className="border row mx-2 align-items-center" key={t.id}>
+                                <ul className="list-group">
+                                    <li className="list-group-item">
+                                        {t.name}
+                                    </li>
+                                </ul>
+                                <span onClick={()=>console.log("")} className="btn btn-sm float-right">
+                                      <DeleteOutlined className="text-danger"/>              
+                                </span>
+                                <Link to={`/update/${t.id}`}>
+                                <span
+                                onClick={()=>console.log("")}
+                                className="btn btn-sm float-right">
+                                <EditOutlined className="text-warning" />
+                                </span>
+                                </Link>
+                            </div>
+                        ))}
+                        </>
+                     )}
+                    </div>
+                </div>
+            </div>
+        
+        );
+};
+
+export default Crud;
