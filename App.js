aa86c192a60f5f76eaa9a25bb17c54c9810b2836
ANKIT GURWAN <109846346+AnkitGurwan@users.noreply.github.com>
2022-10-08 12:30:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import {Routes,Route} from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import {ToastContainer} from "react-toastify";
+import Crud from "./crud";
+import Update from "./crud/Update";
+
+
+function App() {
+  return (
+    <div className="App">    
+      <ToastContainer />        
+      <Routes>
+        <Route exact path="/" element={<Crud/>}/>
+        <Route exact path="/update/:id" element={<Update/>}/>
+      </Routes>      
+    </div>
+  );
+}
+
+export default App;
